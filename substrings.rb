fee5ff123af1aa5272e05f2bd0d3f186a02e556c
jacopaghera <123456789+jacopaghera@gmail.com>
2024-07-14 11:42:48 +0200

    now the number in the hash is the real amount of the words

--- substrings.rb
+++ substrings.rb
@@ -1,11 +1,17 @@
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+require 'pry-byebug'
+
+dictionary = ["below", "below", "down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def substrings(word, sub)
   resultword = Hash.new
   result = 0
     sub.each { |parola|
-      if parola == word.
-        resultword[parola] = result + 1
+      if parola == word
+        result += 1
+        resultword[parola] = result
       end
     }
+  puts resultword
 end
+
+substrings("below", dictionary)
